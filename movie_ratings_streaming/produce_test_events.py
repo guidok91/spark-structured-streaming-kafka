95d3e89dcfc9f66d66f714f0b4427eac82502d79
Guido Kosloff Gancedo <gkg@hellofresh.com>
2022-09-13 15:21:35 +0200

    Produce continuous avro events

--- movie_ratings_streaming/produce_test_events.py
+++ movie_ratings_streaming/produce_test_events.py
@@ -1,9 +1,14 @@
 import logging
 import socket
+import time
+import uuid
+from random import randint, uniform
 
-from confluent_kafka import Producer
+from confluent_kafka import SerializingProducer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
 
-from movie_ratings_streaming.config.config import read_config
+from movie_ratings_streaming.config.config import read_config, read_source_avro_schema
 
 
 def acked(err, msg):
@@ -14,20 +19,38 @@ def acked(err, msg):
 
 
 if __name__ == "__main__":
+    """Ad-hoc script to produce continuous Avro events to the local Kafka broker."""
     logging.basicConfig(level=logging.INFO)
+
     config = read_config()
+    source_avro_schema = read_source_avro_schema()
+    topic = config["stream"]["source_kafka_topic"]
+
+    schema_registry_config = {"url": config["kafka"]["schema.registry.url"]}
+    schema_registry_client = SchemaRegistryClient(schema_registry_config)
+
+    avro_serializer = AvroSerializer(
+        schema_registry_client=schema_registry_client,
+        schema_str=source_avro_schema,
+    )
+
     producer_config = {
         "bootstrap.servers": config["kafka"]["kafka.bootstrap.servers"],
         "client.id": socket.gethostname(),
+        "value.serializer": avro_serializer,
     }
-    topic = config["stream"]["source_kafka_topic"]
+    producer = SerializingProducer(producer_config)
 
-    logging.info(
-        f"Starting Kafka producer with the following config: {producer_config}"
-    )
-    producer = Producer(producer_config)
-
-    logging.info(f"Producing to topic {topic}")
-    producer.produce(topic=topic, value="hahahahaha", callback=acked)
-
-    producer.poll(1)
+    while 1:
+        producer.produce(
+            topic=topic,
+            value={
+                "user_id": str(uuid.uuid1()),
+                "movie_id": str(uuid.uuid1()),
+                "rating": round(uniform(0, 10), 1),
+                "rating_timestamp": randint(1609459200000, 1661990400000),
+            },
+            on_delivery=acked,
+        )
+        producer.poll(1)
+        time.sleep(1)
